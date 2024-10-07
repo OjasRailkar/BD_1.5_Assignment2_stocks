f0901f08d8d358961a30a4785f214c451f20f9dc
OjasRailkar <ojasrailkar3@gmail.com>
2024-10-07 18:27:58 +0530

    updated

--- index.js
+++ index.js
@@ -8,38 +8,52 @@ let port = 3000;
 
 //Endpoint 1: Calculate the Returns of the Stocks added
 
+
 app.get('/calculate-returns', (req, res) => {
   let boughtAt = parseFloat(req.query.boughtAt);
   let marketPrice = parseFloat(req.query.marketPrice);
   let quantity = parseFloat(req.query.quantity);
 
-  let returnValue = (marketPrice - boughtAt) * quantity;
+  let returnValue = (marketPrice - boughtAt) * quantity
   res.send(returnValue.toString());
 });
 
 //Endpoint 2: Calculate the Total Returns
+
+function getTotalReturns(stock1,stock2,stock3,stock4) {
+  return  stock1 + stock2 + stock3 + stock4;
+}
+
 
 app.get('/total-returns', (req, res) => {
   let stock1 = parseFloat(req.query.stock1);
   let stock2 = parseFloat(req.query.stock2);
   let stock3 = parseFloat(req.query.stock3);
   let stock4 = parseFloat(req.query.stock4);
-
-  let totalReturns = stock1 + stock2 + stock3 + stock4;
-  res.send(totalReturns.toString());
+  
+  res.send(getTotalReturns(stock1,stock2,stock3,stock4).toString());
 });
 
 //Endpoint 3: Calculate the Return Percentage
+
+function getReturnPercentage(boughtAt,returns){
+  return (returns / boughtAt) * 100;
+}
+
 
 app.get('/calculate-return-percentage', (req, res) => {
   let boughtAt = parseFloat(req.query.boughtAt);
   let returns = parseFloat(req.query.returns);
 
-  let returnPercentage = (returns / boughtAt) * 100;
-  res.send(returnPercentage.toString());
+  res.send(getReturnPercentage(boughtAt,returns).toString());
 });
 
 //Endpoint 4: Calculate the Total Return Percentage
+
+
+function getTotalReturnPercentage(stock1,stock2,stock3,stock4) {
+  return  stock1 + stock2 + stock3 + stock4;
+}
 
 app.get('/total-return-percentage', (req, res) => {
   let stock1 = parseFloat(req.query.stock1);
@@ -47,19 +61,25 @@ app.get('/total-return-percentage', (req, res) => {
   let stock3 = parseFloat(req.query.stock3);
   let stock4 = parseFloat(req.query.stock4);
 
-  let totalReturnPercentage = stock1 + stock2 + stock3 + stock4;
-  res.send(totalReturnPercentage.toString());
+  res.send(getTotalReturnPercentage(stock1,stock2,stock3,stock4).toString());
 });
+
+
+//Endpoint 5: Identify the Status of Stocks based on their Return Value
+
+function getStockStatus(returnPercentage){
+  let result;
+  if (returnPercentage > 0) {
+    return 'profit';
+  } else {
+    return 'loss';
+  }
+}
 
 app.get('/status', (req, res) => {
   let returnPercentage = parseFloat(req.query.returnPercentage);
-  let result;
-  if (returnPercentage > 0) {
-    result = 'profit';
-  } else {
-    result = 'loss';
-  }
-  res.send(result);
+ 
+  res.send(getStockStatus(returnPercentage).toString());
 });
 
 app.listen(port, () => {
